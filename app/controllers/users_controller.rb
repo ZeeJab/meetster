6fe5d2e17f74f0ca3df53af6d222fdb758288efa
Dustin Coates <dustin@dcoates.com>
2012-12-06 20:39:27 -0500

    Fill out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
 class UsersController < ApplicationController
- 
+
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You've been added. Have a good time!"
+      redirect_to new_user_path
+    else
+      render :new
+    end
+
   end
 
 
   def destroy
+    user = User.find(params[:id])
+    user.delete
   end
 
 end
