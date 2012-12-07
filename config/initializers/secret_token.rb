031a3ca3f457d0a02bce77ce65576907ee8039c3
Dustin Coates <dustin@dcoates.com>
2012-12-06 19:13:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetster::Application.config.secret_token = '3d04c40e4fc4492b8121fb74e403fdeac9f9adb1977b6d069b9bb1bbe5ddedf56ee29439dda7717f849579d35d8b7e7a3ddca46fcac08daab27cfedf4c5be96d'
