67bc7e857f624ff6bee838687109401f1e898f75
Zahra Jabini <zahra.arch@gmail.com>
2012-12-06 20:58:15 -0500

    views changes

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Meetster::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'event#show'
+  root :to => 'events#show'
 
   # See how all your routes lay out with "rake routes"
 
